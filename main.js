027ccfe39faadb389a3fb899b63cebd2f8e439c2
Pedro Cabral <pedrocabralsantos26@gmail.com>
2022-05-06 16:35:14 -0300

    teste

---
+++ main.js
@@ -0,0 +1,100 @@
+setTimeout(function () {
+    window.location.reload(1);
+}, 180000 ); // 3 minutos
+
+
+$.ajax({
+    type: "get", // NOME DO METODO HTTP QUE VAMOS USAR
+    url: "http://192.168.0.23:3333/api/v1/alarm", //URL DE ONDE VOCE VAI BUSCAR
+    success: function (vitirnho) { // SE DER CERTO COMO VAI SRE O NOME DO MEUN
+        vitirnho.map(function (resultado, i) {
+            console.log(resultado)
+            if (resultado.alert_time != null) {
+                if (resultado.dif <= 5) {
+                    document.getElementById("ativos").innerHTML += `
+                    <div onClick="abrir_modal(${resultado.id})" class="botoes_pedro card text-white bg-success mb-3" style="max-width: 15rem;">
+                      <div class="card-header pedro">${resultado.name}</div>
+                      <div class="card-body">
+                          <a style="text-decoration:none; color: white " target="_blank" href="https://wa.me/55${resultado.number}" class=" h3 card-title">${resultado.number}</a>
+                          <p class="card-text">${resultado.dif} Minutos</p>
+                      </div>
+                      </div>
+                    `;              
+                } else {
+                    document.getElementById("inativos").innerHTML += `
+              <div onClick="abrir_modal(${resultado.id})" class="botoes_pedro card text-white bg-danger mb-3" style="max-width: 15rem;">
+                <div class="card-header pedro">${resultado.name}</div>
+                <div class="card-body">
+                    <a style="text-decoration:none ;color: white" target="_blank" href="https://wa.me/55${resultado.number}" class=" h3 card-title">${resultado.number}</a>
+                    <p class="card-text">${resultado.dif} Minutos</p>
+                </div>
+                </div>
+              `;
+                }
+            } else {
+//                document.getElementById("semuso").innerHTML += '<button onClick="abrir_modal('+resultado.id+')" type="button" class="botoes_pedro col-md-3 col-lg-2 col-sm-4 col-xl-2 col-xxl-1 btn btn-warning">' + resultado.number + " <br> <h5>" + resultado.name + " </h8> <br> " + ' <h5> SEM USO </h5></button>';
+
+
+              document.getElementById("semuso").innerHTML += `
+              <div onClick="abrir_modal(${resultado.id})" class="botoes_pedro card text-dark bg-warning mb-3" style="max-width: 15rem;">
+                <div class="card-header pedro">${resultado.name}</div>
+                <div class="card-body">
+                    <a style="text-decoration:none; color: black" target="_blank" href="https://wa.me/55${resultado.number}" class=" h3 card-title">${resultado.number}</a>
+                    <p class="card-text">SEM USO</p>
+                </div>
+                </div>
+              `;
+            }
+        })
+    }
+})
+
+function abrir_modal(id){
+
+    $('#exampleModal').modal('show');     			
+    $.ajax({
+        type: "get", // NOME DO METODO HTTP QUE VAMOS USAR
+        url: "http://192.168.0.23:3333/api/v1/numbers/"+id, //URL DE ONDE VOCE VAI BUSCAR
+        success: function (vitirnho) { // SE DER CERTO COMO VAI SRE O NOME DO MEUN           
+            document.getElementById("numero").value = ''
+            document.getElementById("numero").value = vitirnho.number
+            $.ajax({
+                type: "get", // NOME DO METODO HTTP QUE VAMOS USAR
+                url: "http://192.168.0.23:3333/api/v1/clients/"+vitirnho.client_id, //URL DE ONDE VOCE VAI BUSCAR
+                success: function (vitirnho2) { // SE DER CERTO COMO VAI SRE O NOME DO MEUN           
+                    document.getElementById("nome").value = ''
+                    document.getElementById("nome").value = vitirnho2.name
+                    document.getElementById("botao").innerHTML = `<button type="button" onclick="editar_cliente(${vitirnho.client_id})" class="btn btn-primary">Editar</button>`
+                },
+                error: function (result) {
+                    alert(result.responseJSON.data)
+                }
+            })
+
+        },
+        error: function (result) {
+            alert(result.responseJSON.data)
+        }
+    })
+}
+
+function editar_cliente (id) {
+    const nome = document.getElementById("nome").value
+
+    $.ajax({
+        type: "put", // NOME DO METODO HTTP QUE VAMOS USAR
+        url: "http://192.168.0.23:3333/api/v1/clients/"+id, //URL DE ONDE VOCE VAI BUSCAR
+        data: {
+            name: nome,
+        },
+        success: function (vitirnho) { // SE DER CERTO COMO VAI SRE O NOME DO MEUN
+            console.log(vitirnho)
+            window.location.reload(1);
+        },
+        error: function (result) {
+            alert(result.responseJSON.data)
+        }
+
+    })
+
+}
